d9d72cde72032e9e2fbce29ac2ff291859fda403
kwjung <kwkwjung@gmail.com>
2023-07-05 22:57:01 +0900

    first commit

---
+++ backoffice/src/test/java/backoffice/backoffice/BackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package backoffice.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
